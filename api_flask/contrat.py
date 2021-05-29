13ea21e700fd451422849642f8cc1fbe92a82fb8
Gaetan Jonathan BAKARY <gaetan.jonathan.bakary@esti.mg>
2021-05-30 01:22:21 +0300

    Ajout des Smarts contrats dans l api flask

--- api_flask/contrat.py
+++ api_flask/contrat.py
@@ -1,11 +1,7 @@
-from flask import request, jsonify, Flask
-from datetime import date, datetime
 import json
 from web3 import Web3
 from solcx import compile_standard
 
-
-app = Flask(__name__)
 
 with open('contrats.sol', 'r') as fichier:
     ficVal = fichier.read()
@@ -46,47 +42,19 @@ tx_hash = Vote.constructor().transact()
 
 tx_receipt = web3.eth.waitForTransactionReceipt(tx_hash)
 default_account = tx_receipt.contractAddress
-
+print(abi)
 # default_account = '0x3F2EC437DdDcc487b42c64e0Ff7FD99DA02f46EA'
 vote = web3.eth.contract(
     address=default_account,
     abi=abi
 )
 
+print(vote.functions.insert_user(
+    default_account,
+    'Aze', 'aa', 'aaa', 'aa', 'mdp', 'mail'
+).transact())
 
 
-@app.route('/', methods=['GET'])
-def hello_world():
-    return "<p>Hello, World!</p>"
+az = vote.functions.verifLogin('mail', 'mdp').call()
 
-@app.route('/api/v1/register', methods = ["POST"])
-def register() : 
-    query_parameters = request.form
-    nom = query_parameters.get('nom')
-    prenom = query_parameters.get('prenom')
-    ville = query_parameters.get('ville')
-    adresse = query_parameters.get('adresse')
-    email = query_parameters.get('email')
-    mdp = query_parameters.get('mdp')
-
-    if  (nom and prenom and ville  and adresse and email  and mdp) :       
-        id_ = str(datetime.timestamp(datetime.now())).replace(".", "")
-        #fonction intégrer via solidity pour insérer les données
-        vote.functions.insert_user(
-            id_, nom, prenom, adresse, ville, mdp, email
-        ).transact()
-        return "<p>L'user est ajoutée au base de donnée. </p>", 200
-        
-    else :
-        return "Il y a un élement manquant", 404
-
-
-@app.route('/api/v1/all_user', methods = ["GET"])
-def view_data():
-    return str(vote.functions.all_user().call())
-
-app.run(host='0.0.0.0', port=8082)
-
-
-
-
+print(az)
---
+++ api_flask/contrats.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.16 <0.9.0;
+
+
+
+contract  Webcup{
+
+    struct User {
+        string  id;
+        string  nom;
+        string  prenom;
+        string  adresse;
+        string  ville;
+        string  mdp;
+        string  email;
+    }
+
+    struct Transaction {
+        User user;
+        string type_;
+        string icons;
+        string date_creation;
+    }
+
+    User[] public users;
+    Transaction[] public transaction;
+
+    function compareString(string memory s1, string memory s2) public pure returns(bool){
+        return keccak256(abi.encodePacked(s1)) == keccak256(abi.encodePacked(s2));
+    }
+
+    function all_user() public view returns (User[] memory){
+        return users;
+    }
+
+    function insert_user(string memory id, string memory nom, string memory prenom, string memory adresse, 
+            string memory  ville, string  memory mdp, string  memory  email ) public returns (bool)  {
+
+        users.push(
+            User(id, nom, prenom, adresse, ville, mdp, email)
+        );
+
+        return true;
+    }
+
+    function verifLogin(string memory email, string memory mdp) public view returns(User memory){
+        for (uint i; i<users.length; i++){
+            if (compareString(users[i].email, email) && compareString(users[i].mdp, mdp)){
+                return users[i];
+            }
+        }
+        return User('', '', '', '', '', '', '');
+    }
+
+
+
+    
+    
+
+}
+
