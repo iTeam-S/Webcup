a3a323d00776c126a0b2d1ad22e9e5c7fb0c2ae9
Landris18 <landry.manankoraisina.rasendranirina@esti.mg>
2021-05-30 05:40:31 +0300

    add all

--- api_flask/app.py
+++ api_flask/app.py
@@ -1,29 +1,26 @@
 from flask import request, jsonify, Flask
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
 import json
 from web3 import Web3
 from solcx import compile_standard
 import jwt
+import hashlib
 
-def encode_auth_token(_id):
+def encode_auth_token(user_id):
     """
     Generates the Auth Token
     :return: string
     """
-    try:
-        payload = {
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
-            'iat': datetime.datetime.utcnow(),
-            'sub': user_id
-        }
-        return jwt.encode(
-            payload,
-            app.config.get('SECRET_KEY'),
-            algorithm='HS256'
-        )
-    except Exception as e:
-        return e
-
+    payload = {
+        'exp': datetime.utcnow() + timedelta(days=7),
+        'sub': user_id
+    }
+    return jwt.encode(
+        payload,
+        "MOT_SECRET_DECRYPTE",
+        algorithm='HS256'
+    )
+    
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
@@ -73,11 +70,22 @@ vote = web3.eth.contract(
     abi=abi
 )
 
+def verifToken(token):
+    return jwt.decode(token, options={"verify_signature": False})
 
 
-@app.route('/', methods=['GET'])
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/api/v1/login', methods = ["POST"])
+def login():
+    query_parameters = request.form
+    email = str(query_parameters.get('email'))
+    passwd = hashlib.sha224(str(query_parameters.get('pass')).encode()).hexdigest()
+    res = vote.functions.verifLogin(email, passwd).call()
+    result = { "data" : res, 
+        "id" : res[0],
+        "token" : encode_auth_token(res[0]) if res[0] != '' else ''
+    }, 200 if res[0] != '' else 403
+    return result
+
 
 @app.route('/api/v1/register', methods = ["POST"])
 def register() : 
@@ -87,29 +95,68 @@ def register() :
     ville = query_parameters.get('ville')
     adresse = query_parameters.get('adresse')
     email = query_parameters.get('email')
-    mdp = query_parameters.get('mdp')
+    mdp = hashlib.sha224(str(query_parameters.get('mdp')).encode()).hexdigest()
 
-    if  (nom and prenom and ville  and adresse and email  and mdp) :       
+    if  (nom and prenom and ville  and adresse and email and mdp):
+        recup_user = vote.functions.all_user().call()
+        for usr in recup_user:
+            if usr[6] == email:
+                return "Mail déjà existant", 403
         id_ = str(datetime.timestamp(datetime.now())).replace(".", "")
-        #fonction intégrer via solidity pour insérer les données
         vote.functions.insert_user(
             id_, nom, prenom, adresse, ville, mdp, email
         ).transact()
         result = {
-            'id' : id_ ,
-            'token' : encode_auth_token(id_)
-        }, 
+            "data" : vote.functions.get_user(id_).call(),
+            "id" : id_ ,
+            "token" : encode_auth_token(id_)
+        }
         return result, 200
         
     else :
-        return "Il y a un élement manquant", 404
+        return "Il y a un élement manquant", 403
 
 
 @app.route('/api/v1/all_user', methods = ["GET"])
 def view_data():
-    return str(vote.functions.all_user().call())
+    query_parameters = request.form
+    token = query_parameters.get('token')
+    id_ = query_parameters.get('id')
+    if verifToken(token).get('sub') != id_:
+        return {"status" : "Erreur Token"}, 403
+    return vote.functions.all_user().call()
 
-# @app.route('/')
+
+@app.route('/api/v1/save_transaction', methods = ["POST"])
+def add_transaction():
+    query_parameters = request.form
+    date_creation = str(datetime.now())
+    token = str(query_parameters.get('token'))
+    icons = str(query_parameters.get('icons'))
+    type_ = str(query_parameters.get('type'))
+    id_ = str(query_parameters.get('id'))
+
+    if verifToken(token).get('sub') != id_:
+        return {"status" : "Erreur Token"}, 403
+
+    vote.functions.save_transaction(
+        id_, type_, icons, date_creation
+    ).transact()
+    
+    return 'ok', 200
+
+
+@app.route('/api/v1/transactions', methods = ["POST"])
+def view_transactions():
+    query_parameters = request.form
+    token = query_parameters.get('token')
+    id_ = str(query_parameters.get('id'))
+    if verifToken(token).get('sub') != id_:
+        return {"status" : "Erreur Token"}, 403
+    query_parameters = request.form
+    return {"data": vote.functions.all_transactions().call()}
+
+
 
 app.run(host='0.0.0.0', port=8082)
 
